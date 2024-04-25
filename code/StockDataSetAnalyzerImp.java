167127fb611e9e66bd1d0b120334478f037f3a25
FaisalSWEN <FaisalSWENBusiness@outlook.com>
2024-04-25 04:14:20 +0300

    2/3 StockDataSetAnalyzerImp: getSortedByMSDPI function has been implemented

--- code/StockDataSetAnalyzerImp.java
+++ code/StockDataSetAnalyzerImp.java
@@ -28,8 +28,8 @@ public class StockDataSetAnalyzerImp implements StockDataSetAnalyzer
     for(int i=0; i <companies.size(); i ++, companies.findNext())
     {
       companiesMap.find(companies.retrieve());
-
       StockHistory stockHistory = companiesMap.retrieve();
+
       double stockPerformance = (stockHistory.getStockData(endDate).close - stockHistory.getStockData(startDate).close) / stockHistory.getStockData(startDate).close;
       
       list.insert(new CompPair<String, Double>(companies.retrieve(), stockPerformance));
@@ -52,13 +52,34 @@ public class StockDataSetAnalyzerImp implements StockDataSetAnalyzer
 
 
 
-	// Returns the list of company codes sorted by the maximum single day price
-	// increase between startDate and endDate inclusive. If startDate is null,
-	// fetches from the earliest date. If endDate is null, fetches to the latest
-	// date.
+	// Returns the list of company codes sorted by the maximum single day price increase between startDate and endDate inclusive. -- DONE
+  // If startDate is null, fetches from the earliest date. -- DONE
+  // If endDate is null, fetches to the latest date. -- DONE
 	public DLLComp<CompPair<Pair<String, Date>, Double>> getSortedByMSDPI(Date startDate, Date endDate)
   {
-    
+    DLLComp<CompPair<Pair<String, Date>, Double>> list = new DLLCompImp<CompPair<Pair<String, Date>, Double>>();
+
+    Map<String, StockHistory> companiesMap = companiesStockHistory.getStockHistoryMap();
+    DLLComp<String> companies = companiesMap.getKeys();
+
+    companies.findFirst();
+    for(int i=0; i <companies.size(); i ++, companies.findNext())
+    {
+      companiesMap.find(companies.retrieve());
+      DLL<DataPoint<StockData>> dataPointsStockData = companiesMap.retrieve().getTimeSeries().getDataPointsInRange(startDate, endDate);
+      NumericTimeSeries dataPointsMaxPrice = new NumericTimeSeriesImp();
+
+      dataPointsStockData.findFirst();
+      for(int j=0; j <dataPointsStockData.size(); j ++, dataPointsStockData.findNext())
+        dataPointsMaxPrice.addDataPoint(new DataPoint<Double>(dataPointsStockData.retrieve().date, dataPointsStockData.retrieve().value.high));
+      
+      DataPoint<Double> maxPrice = dataPointsMaxPrice.getMax();
+
+      list.insert(new CompPair<Pair<String, Date>, Double>(new Pair<String, Date>(companies.retrieve(), maxPrice.date), maxPrice.value));
+    }
+
+    list.sort(true);
+    return list;
   }
 
 
